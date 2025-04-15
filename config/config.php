952700a0837bc47002b866690200d8b93bc19228
galeopx <galeopx@gmail.com>
2025-04-15 17:50:07 +0200

    first commit

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Configuration principale de l'application
+ * 
+ * @package Touche Pas Au Klaxon
+ */
+
+// Configuration de base
+define('APP_NAME', 'Touche Pas Au Klaxon');
+define('APP_ROOT', dirname(__DIR__));
+define('URL_ROOT', 'http://localhost/touche_pas_au_klaxon');
+define('URL_SUBFOLDER', '');
+
+// Configuration de la base de données
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'touche_pas_au_klaxon');
